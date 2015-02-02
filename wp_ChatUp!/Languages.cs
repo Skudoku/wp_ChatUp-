da55cb55c08dbba163dc0b2eb3d0ed3132765cd9
Skudoku <ties.baltissen@hotmail.com>
2015-02-02 15:33:27 +0100

    Wijzigingen en zo
    Class toegevoegd

---
+++ wp_ChatUp!/Languages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp_ChatUp_
+{
+    public enum Languages
+    {
+        Dutch,
+        English,
+        French,
+        German,
+        Spanish,
+        Portugese,
+        Italian,
+        Indian
+    }
+}
---
+++ wp_ChatUp!/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp_ChatUp_
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public string Roomname { get; set; }
+        public int Usercount { get; set; }
+        public string Language { get; set; }
+
+
+        public Room(string roomname, string language)
+        {
+            this.Roomname = roomname;
+            this.Language = language;
+        }
+    }
+}
